3cf3b6e4db00a3893379413ad73d4593b7988592
mianguanwu <mianguanwu@gmail.com>
2021-06-17 14:17:55 +0800

    second

--- src/main/java/com/example/superjacoco/util/DateUtil.java
+++ src/main/java/com/example/superjacoco/util/DateUtil.java
@@ -7,5 +7,12 @@ public class DateUtil {
             return true;
         else return false;
     }
+
+    public boolean isLeapYear1(int year)
+    {
+        if((year>0&&year<=10000)&&((year%100!=0&&year%4==0)||(year%400==0)))
+            return true;
+        else return false;
+    }
 }
 
